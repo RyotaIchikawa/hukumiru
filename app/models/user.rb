a7ec4bf059f276b681bf1224e06b7fd9eaee3205
EC2 Default User <ec2-user@ip-172-31-26-65.ap-southeast-1.compute.internal>
2019-03-07 01:19:53 +0000

    add-validates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,14 @@
 class User < ApplicationRecord
+  validates :username, presence: true, length: { maximum: 50 }
+  
+  before_save { self.email = email.downcase }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, length: { maximum: 255 },
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: { case_sensitive: false }
+  
+  validates :password, presence: true, length: { minimum: 6 }
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
