555f53c2def265a0f6e1769a240ec5707120a58f
EC2 Default User <ec2-user@ip-172-31-26-65.ap-southeast-1.compute.internal>
2019-03-12 09:15:46 +0000

    add micropost-model and user-feed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,10 @@
 class User < ApplicationRecord
+  has_many :microposts, dependent: :destroy
   validates :username, presence: true, length: { maximum: 50 }
-  
   before_save { self.email = email.downcase }
   validates :email, presence: true, length: { maximum: 255 },
                     uniqueness: { case_sensitive: false }
-  
   validates :password, presence: true, allow_nil: true
-  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
