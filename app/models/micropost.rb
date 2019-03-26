fe63754f549415f491394a713252cd16a6a4ad23
RyotaIchikawa <ec2-user@ip-172-31-26-65.ap-southeast-1.compute.internal>
2019-03-26 07:38:39 +0000

    add form-select

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -3,7 +3,6 @@ class Micropost < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   mount_uploader :picture, PictureUploader
   validates :user_id, presence: true
-  validates :item_name, presence: true, length: { maximum: 10}
   validates :brand_name, length: { maximum: 15}
   validates :picture, presence: true
   validate  :picture_size
