768afa823a15ff6f4b8efda81ff8f5d3125dac7f
RyotaIchikawa <sugisakisanpass@gmail.com>
2019-04-21 22:53:29 +0900

    fix deleted code in user-controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  before_action :authenticate_user!, only: [:index, :following, :followers]
 
   def index
     @users = User.page(params[:page])
@@ -7,6 +8,20 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
     @microposts = @user.microposts.page(params[:page])
+  end
+
+  def following
+    @title = "Following"
+    @user  = User.find(params[:id])
+    @users = @user.following.page(params[:page])
+    render "show_follow"
+  end
+  
+  def followers
+    @title = "Followers"
+    @user  = User.find(params[:id])
+    @users = @user.followers.page(params[:page])
+    render "show_follow"
   end
 
 end
