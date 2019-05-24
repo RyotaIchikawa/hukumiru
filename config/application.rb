0874766dc4ed0f8ee2e153896fbec96ed4aba694
RyotaIchikawa <sugisakisanpass@gmail.com>
2019-05-25 01:46:02 +0900

    rspec setup

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,17 @@ Bundler.require(*Rails.groups)
 
 module Hukumiru
   class Application < Rails::Application
+    config.load_defaults 5.1
+
     config.i18n.default_locale = :ja 
     config.action_view.embed_authenticity_token_in_remote_forms = true
+
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: false,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false
+    end
   end
 end
